612f29e878b973d8c568fa689e8ec2a734a8f1b6
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-03-15 12:54:25 +0530

    commit

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -12,10 +12,8 @@ const connectDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
+
 
     isConnected = true;
     console.log("MongoDB connected successfully");

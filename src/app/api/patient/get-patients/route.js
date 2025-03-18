e66734fddf6b352d968d069435b77210045e490b
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-03-18 12:05:26 +0530

    commit

--- src/app/api/patient/get-patients/route.js
+++ src/app/api/patient/get-patients/route.js
@@ -4,6 +4,8 @@ import Patient from "@/models/patient";
 
 export async function GET(req) {
   await connectDB();
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // Set time to start of the day
 
   try {
     const patients = await Patient.find({ createdAt: { $gte: today } }).sort({ createdAt: 1 }); // Oldest first

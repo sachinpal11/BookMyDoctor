1f49ef6b0ac6af22c6e967773adf23df6a74db0f
Sachin <100507127+sachinpal11@users.noreply.github.com>
2025-03-18 22:14:02 +0530

    commit

--- src/components/ui/ScheduleTime.jsx
+++ src/components/ui/ScheduleTime.jsx
@@ -1,14 +1,21 @@
 "use client";
 
 import Link from 'next/link'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from './button'
 
 function ScheduleTime({ shifts, shift, setOpen }) {
+  const [display, setdisplay] = useState(true);
+
   useEffect(() => {
-    console.log(shifts);
+    if (shifts[0].startTime === "") {
+      setdisplay(false);
+    }
   }, [])
+
+
   return (
+    display &&
     <div className='w-[90%] rounded-sm bg-gray-100 flex mt-3'>
       <div className='bg-gray-100 text-wrap py-2 w-full h-full flex flex-col items-start px-5 text-xl'>
         <p className=''>
